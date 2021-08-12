1f38fa71b4b4f95a7c4e05375eba53b4339cc180
Daniel Csillag <dccsillag@gmail.com>
2021-08-11 22:16:26 -0300

    Refactor MagmaBuffer and MagmaBuffer.{save,load} into separate modules

---
+++ rplugin/python3/magma/io.py
@@ -0,0 +1,97 @@
+from typing import Type, Optional
+
+from magma.utils        import Span, DynamicPosition
+from magma.outputchunks import OutputStatus, Output, to_outputchunk
+from magma.magmabuffer  import MagmaBuffer
+
+
+class MagmaIOError(Exception):
+    @classmethod
+    def assert_has_key(cls, data: dict, key: str, type_: Optional[Type] = None) -> None:
+        if key not in data:
+            raise cls(f"Missing key: {key}")
+        value = data[key]
+        if type_ is not None and not isinstance(value, type_):
+            raise cls(f"Incorrect type for key '{key}': expected {type_.__name__}, got {type(value).__name__}")
+        return value
+
+
+def load(magmabuffer: MagmaBuffer, data: dict) -> None:
+    MagmaIOError.assert_has_key(data, 'content_checksum', str)
+
+    if magmabuffer._get_content_checksum() != data['content_checksum']:
+        raise MagmaIOError("Buffer contents' checksum does not match!")
+
+    MagmaIOError.assert_has_key(data, 'cells', list)
+    for cell in data['cells']:
+        MagmaIOError.assert_has_key(cell, 'span', dict)
+        MagmaIOError.assert_has_key(cell['span'], 'begin', dict)
+        MagmaIOError.assert_has_key(cell['span']['begin'], 'lineno', int)
+        MagmaIOError.assert_has_key(cell['span']['begin'], 'colno', int)
+        MagmaIOError.assert_has_key(cell['span'], 'end', dict)
+        MagmaIOError.assert_has_key(cell['span']['end'], 'lineno', int)
+        MagmaIOError.assert_has_key(cell['span']['end'], 'colno', int)
+        begin_position = DynamicPosition(
+            magmabuffer.nvim,
+            magmabuffer.extmark_namespace,
+            magmabuffer.buffer.number,
+            cell['span']['begin']['lineno'],
+            cell['span']['begin']['colno']
+        )
+        end_position = DynamicPosition(
+            magmabuffer.nvim,
+            magmabuffer.extmark_namespace,
+            magmabuffer.buffer.number,
+            cell['span']['end']['lineno'],
+            cell['span']['end']['colno']
+        )
+        span = Span(begin_position, end_position)
+
+        # XXX: do we really want to have the execution count here?
+        #      what happens when the counts start to overlap?
+        MagmaIOError.assert_has_key(cell, "execution_count", int)
+        output = Output(cell['execution_count'])
+
+        MagmaIOError.assert_has_key(cell, "status", int)
+        output.status = OutputStatus(cell['status'])
+
+        MagmaIOError.assert_has_key(cell, "success", bool)
+        output.success = cell['success']
+
+        MagmaIOError.assert_has_key(cell, 'chunks', list)
+        for chunk in cell['chunks']:
+            MagmaIOError.assert_has_key(chunk, 'data', dict)
+            MagmaIOError.assert_has_key(chunk, 'metadata', dict)
+            output.chunks.append(to_outputchunk(magmabuffer.runtime._alloc_file, chunk['data'], chunk['metadata']))
+
+        output.old = True
+
+        magmabuffer.outputs[span] = output
+
+
+def save(magmabuffer: MagmaBuffer) -> dict:
+    return {
+        "version": 1,
+        "kernel": magmabuffer.runtime.kernel_name,
+        "content_checksum": magmabuffer._get_content_checksum(),
+        "cells": [
+            {
+                "span": {
+                    "begin": {"lineno": span.begin.lineno, "colno": span.begin.colno},
+                    "end": {"lineno": span.end.lineno, "colno": span.end.colno},
+                },
+                "execution_count": output.execution_count,
+                "status": output.status.value,
+                "success": output.success,
+                "chunks": [
+                    {
+                        "data": chunk.jupyter_data,
+                        "metadata": chunk.jupyter_metadata,
+                    }
+                    for chunk in output.chunks
+                    if chunk.jupyter_data is not None and chunk.jupyter_metadata is not None
+                ],
+            }
+            for span, output in magmabuffer.outputs.items()
+        ],
+    }
---
+++ rplugin/python3/magma/magmabuffer.py
@@ -0,0 +1,288 @@
+from typing import Optional, Dict
+from queue import Queue
+import hashlib
+
+import pynvim
+from pynvim import Nvim
+from pynvim.api import Buffer
+
+from magma.options      import MagmaOptions
+from magma.utils        import MagmaException, Canvas, Position, Span
+from magma.outputchunks import Output, OutputStatus
+from magma.runtime      import JupyterRuntime
+
+
+class MagmaBuffer:
+    nvim: Nvim
+    canvas: Canvas
+    highlight_namespace: int
+    extmark_namespace: int
+    buffer: Buffer
+
+    runtime: JupyterRuntime
+
+    outputs: Dict[Span, Output]
+    current_output: Optional[Output]
+    queued_outputs: 'Queue[Output]'
+
+    display_buffer: Buffer
+    display_window: Optional[int]
+    selected_cell: Optional[Span]
+    should_open_display_window: bool
+    updating_interface: bool
+
+    options: MagmaOptions
+
+    def __init__(self,
+                 nvim: Nvim,
+                 canvas: Canvas,
+                 highlight_namespace: int,
+                 extmark_namespace: int,
+                 buffer: Buffer,
+                 options: MagmaOptions,
+                 kernel_name: str):
+        self.nvim = nvim
+        self.canvas = canvas
+        self.highlight_namespace = highlight_namespace
+        self.extmark_namespace = extmark_namespace
+        self.buffer = buffer
+
+        self.runtime = JupyterRuntime(kernel_name, options)
+
+        self.outputs = {}
+        self.current_output = None
+        self.queued_outputs = Queue()
+
+        self.display_buffer = self.nvim.buffers[self.nvim.funcs.nvim_create_buf(False, True)]
+        self.display_window = None
+        self.selected_cell = None
+        self.should_open_display_window = False
+        self.updating_interface = False
+
+        self.options = options
+
+    def deinit(self):
+        self.runtime.deinit()
+
+    def _buffer_to_window_lineno(self, lineno: int) -> int:
+        win_top = self.nvim.funcs.line('w0')
+        return lineno - win_top + 1
+
+    def run_code(self, code: str, span: Span) -> None:
+        new_output = self.runtime.run_code(code)
+        if span in self.outputs:
+            del self.outputs[span]
+        self.outputs[span] = new_output
+
+        self.queued_outputs.put(new_output)
+
+        self.selected_cell = span
+        self.should_open_display_window = True
+        self.update_interface()
+
+        self._check_if_done_running()
+
+    def reevaluate_cell(self) -> None:
+        self.selected_cell = self._get_selected_span()
+        if self.selected_cell is None:
+            raise MagmaException("Not in a cell")
+
+        code = self.selected_cell.get_text(self.nvim)
+
+        self.run_code(code, self.selected_cell)
+
+    def _check_if_done_running(self) -> None:
+        # TODO: refactor
+        is_idle = self.current_output is None or \
+            (self.current_output is not None and self.current_output.status == OutputStatus.DONE)
+        if is_idle and not self.queued_outputs.empty():
+            output = self.queued_outputs.get_nowait()
+            self.current_output = output
+
+    def tick(self):
+        self._check_if_done_running()
+
+        was_ready = self.runtime.is_ready()
+        did_stuff = self.runtime.tick(self.current_output)
+        if did_stuff:
+            self.update_interface()
+        if not was_ready and self.runtime.is_ready():
+            self.nvim.api.notify(
+                "Kernel '%s' is ready." % self.runtime.kernel_name,
+                pynvim.logging.INFO,
+                {'title': "Magma"},
+            )
+
+    def _get_header_text(self, output: Output) -> str:
+        if output.execution_count is None:
+            execution_count = '...'
+        else:
+            execution_count = str(output.execution_count)
+
+        if output.status == OutputStatus.HOLD:
+            status = '* On Hold'
+        elif output.status == OutputStatus.DONE:
+            if output.success:
+                status = '✓ Done'
+            else:
+                status = '✗ Failed'
+        elif output.status == OutputStatus.RUNNING:
+            status = '... Running'
+        else:
+            raise ValueError('bad output.status: %s' % output.status)
+
+        if output.old:
+            old = "[OLD] "
+        else:
+            old = ""
+
+        return f"{old}Out[{execution_count}]: {status}"
+
+    def _show_outputs(self, output: Output, anchor: Position):
+        # Get width&height, etc
+        win_col = self.nvim.current.window.col
+        win_row = self._buffer_to_window_lineno(anchor.lineno+1)
+        win_width  = self.nvim.current.window.width
+        win_height = self.nvim.current.window.height
+
+        # Clear buffer:
+        self.nvim.funcs.deletebufline(self.display_buffer.number, 1, '$')
+        # Add output chunks to buffer
+        lines = ""
+        lineno = 0
+        shape = (win_col, win_row, win_width, win_height)
+        if len(output.chunks) > 0:
+            for chunk in output.chunks:
+                chunktext = chunk.place(lineno, shape, self.canvas)
+                lines += chunktext
+                lineno += chunktext.count("\n")
+            lines = lines.rstrip().split("\n")
+        self.display_buffer[0] = self._get_header_text(output)
+        self.display_buffer.append(lines)
+
+        # Open output window
+        assert self.display_window is None
+        self.display_window = self.nvim.funcs.nvim_open_win(
+            self.display_buffer.number,
+            False,
+            {
+                'relative': 'win',
+                'col': 0,
+                'row': win_row,
+                'width': win_width,
+                'height': min(win_height - win_row, len(lines)+1),
+                'anchor': 'NW',
+                'style': 'minimal',
+                'focusable': False,
+            }
+        )
+
+    def _get_cursor_position(self) -> Position:
+        _, lineno, colno, _, _ = self.nvim.funcs.getcurpos()
+        return Position(self.nvim.current.buffer.number, lineno-1, colno-1)
+
+    def clear_interface(self) -> None:
+        if self.updating_interface:
+            return
+
+        self.nvim.funcs.nvim_buf_clear_namespace(
+            self.buffer.number,
+            self.highlight_namespace,
+            0,
+            -1,
+        )
+        if self.display_window is not None: # and self.nvim.funcs.winbufnr(self.display_window) != -1:
+            self.nvim.funcs.nvim_win_close(self.display_window, True)
+            self.canvas.clear()
+            self.display_window = None
+
+    def _get_selected_span(self) -> Optional[Span]:
+        current_position = self._get_cursor_position()
+        selected = None
+        for span in reversed(self.outputs.keys()):
+            if current_position in span:
+                selected = span
+                break
+
+        return selected
+
+    def delete_cell(self) -> None:
+        self.selected_cell = self._get_selected_span()
+        if self.selected_cell is None:
+            return
+
+        del self.outputs[self.selected_cell]
+
+        self.update_interface()
+
+    def update_interface(self) -> None:
+        if self.buffer.number != self.nvim.current.buffer.number:
+            return
+        if self.buffer.number != self.nvim.current.window.buffer.number:
+            return
+
+        self.clear_interface()
+
+        self.updating_interface = True
+
+        selected_cell = self._get_selected_span()
+
+        if self.options.automatically_open_output:
+            self.should_open_display_window = True
+        else:
+            if self.selected_cell != selected_cell:
+                self.should_open_display_window = False
+
+        self.selected_cell = selected_cell
+
+        if self.selected_cell is not None:
+            self._show_selected(self.selected_cell)
+        self.canvas.present()
+
+        self.updating_interface = False
+
+    def _show_selected(self, span: Span) -> None:
+        if span.begin.lineno == span.end.lineno:
+            self.nvim.funcs.nvim_buf_add_highlight(
+                self.buffer.number,
+                self.highlight_namespace,
+                self.options.cell_highlight_group,
+                span.begin.lineno,
+                span.begin.colno,
+                span.end.colno,
+            )
+        else:
+            self.nvim.funcs.nvim_buf_add_highlight(
+                self.buffer.number,
+                self.highlight_namespace,
+                self.options.cell_highlight_group,
+                span.begin.lineno,
+                span.begin.colno,
+                -1,
+            )
+            for lineno in range(span.begin.lineno+1, span.end.lineno):
+                self.nvim.funcs.nvim_buf_add_highlight(
+                    self.buffer.number,
+                    self.highlight_namespace,
+                    self.options.cell_highlight_group,
+                    lineno,
+                    0,
+                    -1,
+                )
+            self.nvim.funcs.nvim_buf_add_highlight(
+                self.buffer.number,
+                self.highlight_namespace,
+                self.options.cell_highlight_group,
+                span.end.lineno,
+                0,
+                span.end.colno,
+            )
+
+        if self.should_open_display_window:
+            self._show_outputs(self.outputs[span], span.end)
+
+    def _get_content_checksum(self) -> str:
+        return hashlib.md5(
+            "\n".join(self.nvim.current.buffer.api.get_lines(0, -1, True))
+            .encode("utf-8")
+        ).hexdigest()
